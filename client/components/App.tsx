8bd8b738680bcb67684e26d817156ac406c0465c
JasmineHall1992 <jasminehall5935@gmail.com>
2025-07-31 18:21:25 -0500

    styled the home page with cards, google button, and spot for logo

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import GoogleLoginButton from './GoogleLoginButton';
 import StreamSceneLandingPage from './LandingPage';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
       <StreamSceneLandingPage />
-      <div className="mt-6">
-        <GoogleLoginButton />
       </div>
-    </div>
   );
 };
 
